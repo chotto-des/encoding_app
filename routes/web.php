bff64ad6ea3078f974e8a066a093ea4ca7348c42
Geanne <limpingeanne@gmail.com>
2026-02-24 09:17:54 +0800

    new files/login ui

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Guest routes
+Route::middleware('guest')->group(function () {
+    Route::get('/',          [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login',     [AuthController::class, 'showLogin'])->name('login.page');
+    Route::post('/login',    [AuthController::class, 'login'])->name('login.submit');
+    Route::get('/register',  [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+});
+
+// Authenticated routes
+Route::middleware('auth')->group(function () {
+    Route::get('/home',     fn() => view('home'))->name('home');
+    Route::post('/logout',  [AuthController::class, 'logout'])->name('logout');
 });
