4df84bd14d968ad4fffa35d3a3b8ce3b32225772
ktlsmnss <katemeneses68@gmail.com>
2026-03-03 13:13:20 +0800

    i dunno

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AddressController;
-use Illuminate\Support\Facades\Mail;
-
 
 // Guest routes
 Route::middleware('guest')->group(function () {
@@ -16,37 +14,22 @@ Route::middleware('guest')->group(function () {
     Route::post('/register',      [AuthController::class, 'registerStep1'])->name('register.submit');
     Route::get('/register/step2', [AuthController::class, 'showRegisterStep2'])->name('register.step2');
     Route::post('/register/step2',[AuthController::class, 'register'])->name('register.step2.submit');
-    Route::get('/verify-email',   [AuthController::class, 'showVerification'])->name('verification.show');
-    Route::post('/verify-email',  [AuthController::class, 'verifyOtp'])->name('verification.submit');
-    Route::post('/verify-email/resend', [AuthController::class, 'resendOtp'])->name('verification.resend');
 });
 
 // Authenticated routes
 //Route::middleware('auth')->group(function () {
-    Route::post('/logout',      [AuthController::class, 'logout'])->name('logout');
     Route::get('/home', [StudentController::class, 'index'])->name('home');
     Route::get('/add-student',  [StudentController::class, 'create'])->name('add-student');
     Route::post('/add-student', [StudentController::class, 'store'])->name('add-student.store');
-<<<<<<< HEAD
-<<<<<<< HEAD
     Route::post('/logout',      [AuthController::class, 'logout'])->name('logout');
     Route::delete('/students/{student}',      [StudentController::class, 'destroy'])->name('students.destroy');
     Route::get('/students/{student}/edit',   [StudentController::class, 'edit'])->name('students.edit');
     Route::put('/students/{student}',        [StudentController::class, 'update'])->name('students.update');
-=======
-
-=======
-    Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
-    Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
->>>>>>> 6b8315ad9e65eafffe79fe023e4c48ed9817ea53
-    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
->>>>>>> 507ded4432a75a15c4d4821000448b76fa272529
 //});
 
-// Local address routes (served from DB)
-Route::prefix('address')->group(function () {
+// Local address API (served from DB)
+Route::prefix('api/address')->group(function () {
     Route::get('/provinces',                         [AddressController::class, 'provinces'])->name('address.provinces');
     Route::get('/provinces/{code}/municipalities',   [AddressController::class, 'municipalities'])->name('address.municipalities');
     Route::get('/municipalities/{code}/barangays',   [AddressController::class, 'barangays'])->name('address.barangays');
 });
-
