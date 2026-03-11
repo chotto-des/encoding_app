e7a464a95af4afde8f673fa4c9499b09cee4783a
Geanne <limpingeanne@gmail.com>
2026-03-11 11:19:04 +0800

    minor changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/css/register.css', 'resources/css/site_header.css', 'resources/css/home.css', 'resources/css/add_student.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/css/register.css', 'resources/css/site_header.css', 'resources/css/home_dashboard.css', 'resources/css/add_student.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
