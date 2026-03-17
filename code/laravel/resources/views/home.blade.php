4c90ddef97d0118000c587db0cf05bbc1d2260fd
ktlsmnss <katemeneses68@gmail.com>
2026-03-17 14:54:42 +0800

    updated ui home

--- code/laravel/resources/views/home.blade.php
+++ code/laravel/resources/views/home.blade.php
@@ -34,12 +34,13 @@
 		<div style="width: 100%; border-bottom: 10px solid #EEEE3D; box-shadow: 0 -6px 12px rgba(0, 0, 0, 0.3);"></div>
 
 		<div class="row" style="background-color: #f8f7d8;">
- 			<div class=" col-12 col-sm-6 col-md-4 col-lg-3 m-5 mx-auto z-20 d-flex justify-content-center align-items-center">	
+			
+ 			<div class=" col-12 col-sm-6 col-md-4 col-lg-3 z-20 my-5 margin-card d-flex justify-content-center align-items-center">	
 				
 				<a href="{{ route('students.index') }}" class="h-100 card text-decoration-none text-dark border-gold pe-1 pt-3 pb-3 ps-3 rounded-4">
 					<div class="card__icon">
 						<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" fill="#FFD700" class="bi bi-folder-fill p-3	 rounded-4 ms-3 mt-3" style="background-color: #f8f7d8;" viewBox="0 0 16 16">
-						<path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/>
+						<path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172_2H2.5a1_1_0_0_0-1_.981z"/>
 						</svg>			
 					</div>
 
@@ -56,12 +57,12 @@
 				</a>
 			</div>
 
-			<div class=" col-12 col-sm-6 col-md-4 col-lg-3 m-5 mx-auto z-20 d-flex justify-content-center align-items-center">	
+			<div class=" col-12 col-sm-6 col-md-4 col-lg-3 my-5 z-20 d-flex justify-content-center align-items-center">	
 				
 				<a href="{{ route('students.index') }}" class="h-100 card text-decoration-none text-dark border-gold pe-1 pt-3 pb-3 ps-3 rounded-4">
 					<div class="card__icon">
 						<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" fill="#FFD700" class="bi bi-folder-fill p-3	 rounded-4 ms-3 mt-3" style="background-color: #f8f7d8;" viewBox="0 0 16 16">
-						<path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/>
+						<path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172_2H2.5a1_1_0_0_0-1_.981z"/>
 						</svg>			
 					</div>
 
